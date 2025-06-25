09f3c0a0896762c0b193ab9757ea04ea3b907634
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-06-25 17:39:23 +0530

    Refactor: Clean up import statements in layout.tsx files

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,7 @@
 import { isAuthenticated } from '@/lib/actions/auth.action';
 import { redirect } from 'next/navigation';
+// import { ReactNode } from "react";
+// import { redirect } from "next/navigation";
 
 const AuthLayout = async ({children}:{children:React.ReactNode}) => {
   const isUserAuthenticated = await isAuthenticated(); // Replace with actual authentication logic
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
 
-import { isAuthenticated } from '@/lib/actions/auth.action';
-import Image from 'next/image'
-import Link from 'next/link'
-import {redirect} from 'next/navigation'
+import { isAuthenticated } from "@/lib/actions/auth.action";
 
 const RootLayout = async ({children} : {children: React.ReactNode}) => {
 
-  const isUserAuthenticated = await isAuthenticated(); // Replace with actual authentication logic
+  const isUserAuthenticated = await isAuthenticated(); 
   if (!isUserAuthenticated) {
-    redirect('/');
+    redirect('/sign-in');
   }
 
   return (
