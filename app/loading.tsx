9ad383636b8f0a24bec19fbb1d3a293adfe9d6ce
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-12-25 14:30:45 +0530

    Add README.md with project overview, folder structure, tech stack, and getting started instructions; implement Loading component for loading state

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <h2>Loading...</h2>
+    </div>
+  )
+}
+export default Loading
